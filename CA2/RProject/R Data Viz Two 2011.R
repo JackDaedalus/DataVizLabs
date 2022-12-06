b4f2cb4e88f347729b15c588b6a64b03e74ce398
Ciaran Finnegan <ciaran@feefinnegan.com>
2022-12-06 17:23:44 +0000

    DataViz 2 - first plot

--- CA2/RProject/R Data Viz Two 2011.R
+++ CA2/RProject/R Data Viz Two 2011.R
@@ -29,72 +29,95 @@ library(dplyr)
 # Load Census Theme Data for 2011 for Irish counties
 # Select only the required Socio-economic data 
 # Rename the columns to increase understanding of the data
-# df2011CountyThemes <- read_delim("https://www.cso.ie/en/media/csoie/census/documents/saps2011files/AllThemesTablesCTY.csv",show_col_types = FALSE) %>%
-#  select(GEOGID, GEOGDESC, T8_1_LFFJT, T8_1_ULGUPJT, T8_1_TT) %>%
-#  rename(Looking_for_Work = T8_1_LFFJT, Unemployed = T8_1_ULGUPJT, Total_Workforce = T8_1_TT)
+df2011CountySocioThemes <- read_delim("https://www.cso.ie/en/media/csoie/census/documents/saps2011files/AllThemesTablesCTY.csv",
+                                 show_col_types = FALSE) %>%   # Read Census 2011 data from CSO website
+  select(GEOGID, GEOGDESC, # Only select the county identifier and the numbers of people in each
+          T9_2_PA,         # Socio-economic group
+          T9_2_PB,
+          T9_2_PC,
+          T9_2_PD,
+          T9_2_PE,
+          T9_2_PF,
+          T9_2_PG,
+          T9_2_PH,
+          T9_2_PI,
+          T9_2_PJ,
+          T9_2_PZ) %>%
+  rename(GroupA = T9_2_PA,  # Rename Columns to improve readability
+         GroupB = T9_2_PB,
+         GroupC = T9_2_PC,
+         GroupD = T9_2_PD,
+         GroupE = T9_2_PE,
+         GroupF = T9_2_PF,
+         GroupG = T9_2_PG,
+         GroupH = T9_2_PH,
+         GroupI = T9_2_PI,
+         GroupJ = T9_2_PJ,
+         GroupZ = T9_2_PZ)
 
-
-df2011CountyThemes <- read_delim("https://www.cso.ie/en/media/csoie/census/documents/saps2011files/AllThemesTablesCTY.csv",
-                                 show_col_types = FALSE) %>%
-  select(GEOGID, GEOGDESC, 
-          T9_2_HA,
-          T9_2_HB,
-          T9_2_HC,
-          T9_2_HD,
-          T9_2_HE,
-          T9_2_HF,
-          T9_2_HG,
-          T9_2_HH,
-          T9_2_HI,
-          T9_2_HJ,
-          T9_2_HZ) %>%
-  rename(GroupA = T9_2_HA,
-         GroupB = T9_2_HB,
-         GroupC = T9_2_HC,
-         GroupD = T9_2_HD,
-         GroupE = T9_2_HE,
-         GroupF = T9_2_HF,
-         GroupG = T9_2_HG,
-         GroupH = T9_2_HH,
-         GroupI = T9_2_HI,
-         GroupJ = T9_2_HJ,
-         GroupZ = T9_2_HZ)
-
-View(df2011CountyThemes)
+View(df2011CountySocioThemes)
 
 
 
 
 # Sum County Data into a single row
 
+# Group the counties and sum all socio-economic data for Ireland overall
+df2011CtyThemesSocioTotals <- sqldf("Select 'CTT' as GEOGID,
+                                    '2011' as Year,
+                                     sum(GroupA) as GrpA,
+                                     sum(GroupB) as GrpB,
+                                     sum(GroupC) as GrpC,
+                                     sum(GroupD) as GrpD,
+                                     sum(GroupE) as GrpE,
+                                     sum(GroupF) as GrpF,
+                                     sum(GroupG) as GrpG,
+                                     sum(GroupH) as GrpH,
+                                     sum(GroupI) as GrpI,
+                                     sum(GroupJ) as GrpJ,
+                                     sum(GroupZ) as GrpZ
+                                     from df2011CountySocioThemes
+                                     group by Year")
+
+head(df2011CtyThemesSocioTotals)
+str(df2011CtyThemesSocioTotals)
+
 
 
 # Pivot County Data for numbers in Each Socio-economic Group
+df2011CtyThemesSocioTotals_Reshape <- df2011CtyThemesSocioTotals %>% 
+  pivot_longer(c(GrpA,
+                 GrpB,
+                 GrpC,
+                 GrpD,
+                 GrpE,
+                 GrpF,
+                 GrpG,
+                 GrpH,
+                 GrpI,
+                 GrpJ,
+                 GrpZ), # values to pivot or reshape
+               names_to = "SocioEcon_Group", # Rename column for exam type (written or oral)
+               values_to = "Numbers_in_Group") # Re-name column containing the exam scores
+
+view(df2011CtyThemesSocioTotals_Reshape)
+str(df2011CtyThemesSocioTotals_Reshape)
 
 
 
-# Add column to identify numbers in each socio-economic group
-
-
-
-
-# Plot 2011 data in isolation - temporary graph
-ggplot(dfCountyMap) + 
-  geom_polygon(colour="black", aes(x=long, y=lat, group=group, fill=Unemploy_Pct)) + 
-  labs(x = NULL, y = NULL,                                                          
-       title = "Unemployment Rate by County (2011) for Ireland",
+# Plot socio-economic groups
+gg1 <- ggplot(data=df2011CtyThemesSocioTotals_Reshape, aes(x=SocioEcon_Group, y=Numbers_in_Group, fill=Year)) +
+  geom_bar(stat="identity", position=position_dodge(), colour="black") +
+  labs(x = "Socio-Economic Groups", y = "Population (numbers)",                                                          
+       title = "Breakdown of Socio-Economic Groups (2011) for Ireland",
        subtitle = "Sources: Census 2011", 
        caption = "Plot by C.Finegan d21124026") +  
-  theme(axis.line=element_blank(), 
-        axis.ticks=element_blank(), 
-        axis.text=element_blank(),
-        axis.title=element_blank(),
-        panel.grid = element_blank(),
-        plot.caption.position = 'plot',
-        plot.title.position = 'plot',
-        legend.position = "bottom") + 
-  #scale_fill_brewer(palette="OrRd") + 
-  labs(fill = "Unemployment Rate (%)")+
-  scale_fill_brewer(palette="YlOrRd")
+  theme(legend.position = "right",
+        plot.title = element_text(size = 17),
+        axis.title = element_text(size = 15),
+        axis.text.x = element_text(size=12, face="bold", colour = "black")) + 
+  scale_fill_discrete(labels=c('2011', '2016'),name = "Year") +
+  coord_flip()
+print(gg1)
 
 
